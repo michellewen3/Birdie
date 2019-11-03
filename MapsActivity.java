41d4d5bffd9c69b0dea0041f0fbffc2f87edc82b
Michelle Wen <45675722+michellewen3@users.noreply.github.com>
2019-11-02 20:28:35 -0500

    Add files via upload

--- MapsActivity.java
+++ MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.maptest;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -12,8 +13,13 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,6 +28,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ScrollView;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -45,10 +53,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-        Button button1= (Button)findViewById(R.id.button);
+        Button button1= (Button)findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(getApplicationContext(), "Yay", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Yay!!!!", Toast.LENGTH_SHORT).show();
                 //startActivity(new Intent(MapsActivity.this,MapsActivity.class));
             }
         });
@@ -68,9 +76,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
+        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+            .add(
+                    new LatLng(-35.016, 143.321),
+                    new LatLng(-34.747, 145.592),
+                    new LatLng(-34.364, 147.891),
+                    new LatLng(-33.501, 150.217),
+                    new LatLng(-32.306, 149.248),
+                    new LatLng(-32.491, 147.309))
+            .width(10)
+            .color(Color .BLUE)
+            .startCap(new RoundCap())
+            .jointType(JointType .ROUND)
+            .endCap(new RoundCap())
+            );
+
+        //
+
+
+        //(polyline1);
+
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        /*LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney1 = new LatLng(-35, 152);
+        LatLng sydney2 = new LatLng(-35, 152);
+        LatLng sydney3 = new LatLng(-35, 152);
+        LatLng sydney4 = new LatLng(-35, 152);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("1"));
+        mMap.addMarker(new MarkerOptions().position(sydney1).title("2"));
+        mMap.addMarker(new MarkerOptions().position(sydney2).title("3"));
+        mMap.addMarker(new MarkerOptions().position(sydney3).title("4"));
+        mMap.addMarker(new MarkerOptions().position(sydney4).title("5"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        */
+
     }
+
+    /*private void stylePolyline(Polyline polyline) {
+        polyline.setEndCap(new RoundCap());
+        polyline.setColor(0x1E90FF);
+        polyline.setWidth(100);   //number in pixels
+        polyline.setJointType(JointType.ROUND);
+    }*/
+
+
+
+
 }
