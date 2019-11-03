ea8c4c2911811e06e9ed97025c7e853d36de32fd
Isaac <44151664+Zentimenthol@users.noreply.github.com>
2019-11-02 21:50:48 -0500

    Add files via upload

--- MapsActivity.java
+++ MapsActivity.java
@@ -1,67 +1,112 @@
-package com.example.maptest;
+package com.example.test3;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityEventSource;
-
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.JointType;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.maps.model.RoundCap;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
-import android.widget.ScrollView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    double latitude, longitude;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
+    Timer timer = new Timer();
+
+    List<LatLng> points = new ArrayList<>(); // list of latlng
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        //new
-        Button button= (Button)findViewById(R.id.button);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        Button button= findViewById(R.id.Start);
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(getApplicationContext(), "Yay", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Tracking Started", Toast.LENGTH_SHORT).show();
+                fetchLocation();
+                timer.scheduleAtFixedRate(new TimerTask(){
+                    @Override
+                    public void run() {
+                        fetchLocation();//Called each time when 1000 milliseconds (1 second) (the period parameter)
+                    }
+                    },0,5000);
+                //Toast.makeText(getApplicationContext(), "Tracking Started", Toast.LENGTH_SHORT).show();
                 //startActivity(new Intent(MapsActivity.this,MapsActivity.class));
             }
         });
-
-        Button button1= (Button)findViewById(R.id.button1);
+        Button button1= findViewById(R.id.Stop);
+        button1.setVisibility(View.VISIBLE);
         button1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(getApplicationContext(), "Yay!!!!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Ending Track", Toast.LENGTH_SHORT).show();
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 13));
+                timer.cancel();
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(latitude, longitude))      // Sets the center of the map to location user
+                        .zoom(17)                   // Sets the zoom
+                        .bearing(90)                // Sets the orientation of the camera to east
+                        .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                        .build();                   // Creates a CameraPosition from the builder
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
                 //startActivity(new Intent(MapsActivity.this,MapsActivity.class));
             }
         });
     }
-
+    private void fetchLocation(){
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    Toast.makeText(getApplicationContext(), "Attempt no: " + i + " " + currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    latitude = currentLocation.getLatitude();
+                    longitude = currentLocation.getLongitude();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    assert supportMapFragment != null;
+                    supportMapFragment.getMapAsync(MapsActivity.this);
+                    i++;
+                }
+            }
+        });
+    }
 
     /**
      * Manipulates the map once available.
@@ -76,52 +121,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-
-        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
-            .add(
-                    new LatLng(-35.016, 143.321),
-                    new LatLng(-34.747, 145.592),
-                    new LatLng(-34.364, 147.891),
-                    new LatLng(-33.501, 150.217),
-                    new LatLng(-32.306, 149.248),
-                    new LatLng(-32.491, 147.309))
-            .width(10)
-            .color(Color .BLUE)
-            .startCap(new RoundCap())
-            .jointType(JointType .ROUND)
-            .endCap(new RoundCap())
-            );
-
-        //
-
-
-        //(polyline1);
-
-
         // Add a marker in Sydney and move the camera
-        /*LatLng sydney = new LatLng(-34, 151);
-        LatLng sydney1 = new LatLng(-35, 152);
-        LatLng sydney2 = new LatLng(-35, 152);
-        LatLng sydney3 = new LatLng(-35, 152);
-        LatLng sydney4 = new LatLng(-35, 152);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("1"));
-        mMap.addMarker(new MarkerOptions().position(sydney1).title("2"));
-        mMap.addMarker(new MarkerOptions().position(sydney2).title("3"));
-        mMap.addMarker(new MarkerOptions().position(sydney3).title("4"));
-        mMap.addMarker(new MarkerOptions().position(sydney4).title("5"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        */
+        LatLng coor = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        points.add(coor);
 
+        /**for (int i = 0; i < points.size() - 1; i++) {
+            LatLng src = points.get(i);
+            LatLng dest = points.get(i + 1);
+
+            // mMap is the Map Object
+            Polyline line = mMap.addPolyline(
+                    new PolylineOptions().add(
+                            new LatLng(src.latitude, src.longitude),
+                            new LatLng(dest.latitude,dest.longitude)
+                    ).width(15).color(Color.BLUE).startCap(new RoundCap()).jointType(JointType .ROUND).endCap(new RoundCap()).geodesic(true)
+            );
+        }*/
+
+        mMap.addMarker(new MarkerOptions().position(coor).title("Currently Here"));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(coor));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(coor));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coor, 17));
     }
-
-    /*private void stylePolyline(Polyline polyline) {
-        polyline.setEndCap(new RoundCap());
-        polyline.setColor(0x1E90FF);
-        polyline.setWidth(100);   //number in pixels
-        polyline.setJointType(JointType.ROUND);
-    }*/
-
-
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLocation();
+                }
+                break;
+        }
+    }
 
 }
